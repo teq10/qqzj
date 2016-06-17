8449a8497311b6a0b9332d52958994af54eae76f
teq10 <teq0226@163.com>
2016-06-17 22:14:05 +0800

    s

--- handler/oauth.py
+++ handler/oauth.py
@@ -85,6 +85,7 @@ class WxOauthHandler(BaseHandler):
             self.render("error.html", message="get wxuser failed!")
             return
         #print wxuser['openid']
+        print "hehe"
         self.set_secure_cookie("userid",wxuser['openid'])
         user = self.db.get("SELECT id FROM user WHERE openid = %s", wxuser['openid'])
 
@@ -95,6 +96,7 @@ class WxOauthHandler(BaseHandler):
                         ,wxuser['openid'])
 
         #jingweidu
+
         self.redirect((Const.URL_SERVICE)%service)
 
 
