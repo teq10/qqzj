25778f39054d34d6fbd9e581d1506c855569471c
teq10 <teq0226@163.com>
2016-06-17 21:37:53 +0800

    s

--- handler/oauth.py
+++ handler/oauth.py
@@ -19,7 +19,7 @@ class WxOauthHandler(BaseHandler):
         openid = self.get_secure_cookie("openid")
         if openid:
             #jingweidu
-            self.redirect(Const.URL_SERVICE) % (service)
+            self.redirect(Const.URL_SERVICE) % service
         else:
 
 
