5d2de2f511c1ec178f8c26787585eb53aa777d46
teq10 <teq0226@163.com>
2016-09-06 20:23:23 +0800

    d

--- handler/weixin.py
+++ handler/weixin.py
@@ -94,9 +94,12 @@ class WeixinHandler(BaseHandler):
 
 
         weixinid = msg['FromUserName']
-        '''
+
         Latitude = msg['Latitude']
         Longitude = msg['Longitude']
+        print Latitude,Longitude
+        '''
+        
         user = self.db.get("SELECT id FROM user WHERE openid = %s", weixinid)
 
         if not user:
