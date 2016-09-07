60336a72a4be1bfce70f6e2df586bcc8822fd396
teq10 <teq0226@163.com>
2016-09-07 09:16:01 +0800

    r

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@ from tornado.options import define, options
 
 define("port", default=9002, help="run on the given port", type=int)
 
-define("mysql_host", default="123.56.154.165", help="blog database host")
+define("mysql_host", default="localhost", help="blog database host")
 define("mysql_database", default="wxTest", help="blog database name")
 define("mysql_user", default="root", help="root database user")
 define("mysql_password", default="cims603", help="root database password")
